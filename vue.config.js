634d15db83a966124f1bbaefd9ad9220e0bb4945
LeeKyeongHwan <rudghks7816@gmail.com>
2019-10-11 22:48:44 +0900

    github pages > docs폴더 추가

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 // vue.config.js
 module.exports = {
   lintOnSave: process.env.NODE_ENV !== 'production',
+  publicPath: process.env.NODE_ENV === 'production' ? '/terafunding-test/' : '/',
+  outputDir: 'docs',
   css: {
     loaderOptions: {
       sass: {
